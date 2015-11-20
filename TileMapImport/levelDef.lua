2e84d7b23434fca989f9801da6320d50e4122b4f
Joel Hill <joel.hill.87@gmail.com>
2015-11-20 15:44:55 -0600

    added start position to level definition

--- TileMapImport/levelDef.lua
+++ TileMapImport/levelDef.lua
@@ -4,9 +4,11 @@ Level = {}
 
 --This function is added to the table
 -- Returns a new level instance with a tilemap table and image path
-Level.New = function(map, path)
+Level.New = function(map, path, startX, startY)
 	local self = {}
 	self.tilemap = map
 	self.imagePath = path
+	self.startX = startX
+	self.startY = startY
 	return self
 end
